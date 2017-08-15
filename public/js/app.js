2f0ea59b9455f10e41f3e9db71412a03273860fd
alexandraalday <alexandra.alday@gmail.com>
2017-08-14 22:25:58 -0700

    commit 35: set up angular controller for game search

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
-$(()=>{
+const app = angular.module("Goodgame", []);
 
+app.controller("SearchController", ['$http', function($http){
 
+this.find = function(){
+	const controller = this;
+	$http({
+		method: "GET",
+		url: "http://jservice.io/api/random"
+	}).then(
+		function(response){
+			console.log(response.data); 
+			controller.data = response.data 
+	}, 
+		function(err){
+			console.log(err);
+		}
+	)
+	}
 
-
-
-
-
-
-
-})
+}]);
