9735d6c731928bb390ec8406b1caff6567b10fab
alexandraalday <alexandra.alday@gmail.com>
2017-11-07 21:59:38 -0800

    refactor main and app js

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,24 @@
-console.log('loaded bro');
-
 const app = angular.module("Goodgame", []);
 
-	// whitelist api address to use result links
-	  app.config(['$sceDelegateProvider', function($sceDelegateProvider) {
-	    $sceDelegateProvider.resourceUrlWhitelist([
-	        'self',
-	        'http//images.igdb.com**'
-	    ]);
-	  }]);
+	app.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+    	$sceDelegateProvider.resourceUrlWhitelist([
+        	'self',
+        	'http//images.igdb.com**'
+    	]);
+  	}]);
 
 	app.controller("SearchController", ['$http', function($http){
 		let controller = this;
 		this.url = 'http://localhost:3000'
-		this.searchResults = [];     //This holds list of games from search
+		this.searchResults = [];
 	    
-	    this.find = function(searchData) {   //This function makes request from user input
-	    	console.log(searchData)
+	    this.find = function(searchData) {
 	        $http ({
 	            method: 'GET',
 	            url: this.url + '/games/searchResult/:' + searchData
 	        }).then(function(response) {
 	            controller.searchResults = response.data;
-	            console.log(controller.searchResults);
 	        })
 	    };
 
-}]);
+	}]);
--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,48 @@
 $(document).ready(function() {
 
-$('.gameSlider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  centerMode: true,
-  variableWidth: true,
-    responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+  $('.gameSlider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
-      
-
-let $grid = $('.grid').imagesLoaded( function(){
-  $grid.masonry({
-    itemSelector: '.portfolio-item',
-    columnWidth: 300,
-    isAnimated: csstransitions,
-    easing: 'linear' 
+      ]
   });
-});
+        
 
+  let $grid = $('.grid').imagesLoaded(function(){
+    $grid.masonry({
+      itemSelector: '.portfolio-item',
+      columnWidth: 300,
+      isAnimated: csstransitions,
+      easing: 'linear' 
+    });
+  });
 
 
 });  
